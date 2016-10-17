34092be90d8ad60e6e16a997f1ae3a533063e9c9
raziyeh bazargan <r.bazargan@gmail.com>
2016-10-17 14:42:48 -0700

    first Bonus - copy current state

---
+++ lab-raziyeh/app/entry.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//require webpack assets
+require('./scss/base.scss');
+
+//npm modules
+const cowsay = require('cowsay-browser');
+const angular = require('angular');
+
+//app modules
+
+//angular module
+const demoApp = angular.module('demoApp', []);
+
+//angular constructors
+demoApp.controller('CowsayController', ['$log','$scope', CowsayController]);
+
+function CowsayController($log, $scope) {
+  $log.debug('init CowsayController');
+
+  let cowsayCtrl = $scope.cowsayCtrl = {};
+  cowsayCtrl.title = 'Moooooooo';
+  cowsayCtrl.show = false;
+  cowsayCtrl.state  = null;
+
+  cowsayCtrl.updateCow = function(input) {
+    $log.debug('cowsayCtrl.updateCow()');
+    
+    return '\n' + cowsay.say({text: input || 'gimme something to say'});
+  };
+  cowsayCtrl.copyCow = function(input) {
+    $log.debug('cowsayCtrl.copyCow()');
+
+    cowsayCtrl.state = '\n' + cowsay.say({text: input});
+    return cowsayCtrl.show === false ? cowsayCtrl.show = true : cowsayCtrl.show = false;
+  };
+
+}
+
